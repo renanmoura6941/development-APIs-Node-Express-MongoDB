b4e26187760952ffa653d9433d5b1d644fbf69a2
renanmoura6941 <naneraruom@gmail.com>
2021-03-02 08:42:52 -0300

    put

--- express-project/src/routes.js
+++ express-project/src/routes.js
@@ -7,13 +7,12 @@ module.exports = routes;
 
 const products = []
 
-routes.get('/',(req,res)=>{
+routes.get('/products',(req,res)=>{
     return res.json(products);
 })
 
-routes.post('/products', (req, res)=>{
+routes.post('/products/', (req, res)=>{
     const {name, description, price , category} = req.body;
-    
     const product = {
         id:uuid(),
         name,
@@ -22,14 +21,41 @@ routes.post('/products', (req, res)=>{
         category,
     }
     products.push(product)
-    
     return res.json(products)
 })
 
-routes.put('/', (req, res)=>{
-    return res.json({message:'put'})
+routes.put('/products/:id', (req, res)=>{
+    const {id} = req.params;
+    const {name, description, price , category} = req.body;
+
+    const productIndex = products.findIndex(element=> element.id == id)
+
+    if(productIndex == -1){
+        return res.status(400).json({error:"Product does not found"})
+    }
+
+    products.splice(productIndex,1 );
+
+    const product = {
+        id:uuid(),
+        name,
+        description,
+        price,
+        category,
+    }
+    products.push(product)
+
+    return res.status(204).send();
 })
 
-routes.delete('/', (req, res)=>{
+routes.delete('/products/:id', (req, res)=>{
+    const {id} = req.params;
+
+    const productIndex = products.findIndex(product=> product.id == id)
+
+    if(productIndex == -1){
+        return res.status(400).json({error:"Product does not found"})
+    }
+
     return res.json({message:'delete'})
 })
--- express-project/src/server.js
+++ express-project/src/server.js
@@ -1,13 +1,13 @@
 const { json } = require("express");
-const express = require("express")
-const routes = require("./routes")
+const express = require("express");
+const routes = require("./routes");
 const app = express();  
 
 
 //body type json
-app.use(express.json())
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
 
-app.listen(3333)
+app.listen(3333);
