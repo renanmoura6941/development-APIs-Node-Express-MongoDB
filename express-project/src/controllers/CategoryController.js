2a6e88dd10c03b6ef9e3f06717b01ae540ec8087
renanmoura6941 <naneraruom@gmail.com>
2021-03-05 10:23:36 -0300

    update

--- express-project/src/controllers/CategoryController.js
+++ express-project/src/controllers/CategoryController.js
@@ -1,7 +1,14 @@
+import { json } from 'express';
 import Category from '../models/Category';
 
 class CategoryController {
 
+    async index(req, res) {
+        const categories = await Category.find({});
+        return res.json(categories);
+    }
+
+    //criar uma nova categoria
     async store(req, res) {
 
         const { name } = req.body;
@@ -9,6 +16,22 @@ class CategoryController {
         const category = await Category.create({ name });
 
         return res.json(category);
+    }
+
+    //atualizar
+    async update(req, res) {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        const category = await Category.findById(id);
+
+        if (!category) {
+            return res.status(400).json({ error: 'Category does not found' });
+        }
+        category.name = name;
+        await category.save();
+        return res.json(category);
+
     }
 }
 
--- express-project/src/routes.js
+++ express-project/src/routes.js
@@ -5,73 +5,7 @@ import CategoryController from './controllers/CategoryController'
 const routes = new Router();
 
 routes.post('/categories', CategoryController.store);
-
-export default routes;
-
-
-
-
-
-
-
-
-
-
-const products = []
-
-routes.get('/products', (req, res) => {
-    return res.json(products);
-})
-
-routes.post('/products/', (req, res) => {
-    const { name, description, price, category } = req.body;
-    const product = {
-        id: uuid(),
-        name,
-        description,
-        price,
-        category,
-    }
-    products.push(product)
-    return res.status(201).send();
-})
-
-routes.put('/products/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, description, price, category } = req.body;
-
-    const productIndex = products.findIndex(element => element.id == id)
-
-    if (productIndex == -1) {
-        return res.status(400).json({ error: "Product does not found" })
-    }
-
-    products.splice(productIndex, 1);
-
-    const product = {
-        id: uuid(),
-        name,
-        description,
-        price,
-        category,
-    }
-    products.push(product)
-
-    return res.status(200).send();
-})
-
-routes.delete('/products/:id', (req, res) => {
-    const { id } = req.params;
-
-    const productIndex = products.findIndex(product => product.id == id)
-
-    if (productIndex == -1) {
-        return res.status(400).json({ error: "Product does not found" })
-    }
-
-    products.splice(productIndex, 1);
-
-    return res.status(204).send();
-})
+routes.get('/categories', CategoryController.index);
+routes.put('/categories/:id', CategoryController.update);
 
 export default routes;
