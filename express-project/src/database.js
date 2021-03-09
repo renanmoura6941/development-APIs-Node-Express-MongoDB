b6cd2e2d3c2bca762cc0fc63f7b69b49171a99d3
renanmoura6941 <naneraruom@gmail.com>
2021-03-09 12:33:15 -0300

    padrão

--- express-project/src/database.js
+++ express-project/src/database.js
@@ -3,6 +3,17 @@ import mongoose from 'mongoose';
 
 class Database{
 
+    constructor(){
+        this.init();
+    }
+
+    init(){
+        mongoose.connect('mongodb://localhost:27017/userdb',
+            {useNewUrlParser:true, useUnifiedTopology:true}
+        );
+
+    }
+
 }
 
 export default new Database();
